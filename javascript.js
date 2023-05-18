93aed861e33e9e9c540d395b376408a41c83e358
Magdalena Rozpara <magda.rozpara96@gmail.com>
2023-05-18 07:30:01 +0200

    Add computersChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("hello")
+// FIRST APPROACH:
+
+function getComputersChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const computersChoiceIndex = Math.floor(Math.random() * choices.length);
+    console.log(computersChoiceIndex)
+    const computersChoice = choices[computersChoiceIndex];
+    
+    return computersChoice;
+}
+
+console.log(getComputersChoice())
+
+function getUsersChoice() {
+    let usersChoice = prompt("Please enter 'rock', 'paper' or 'scissors':");
+    let usersChoiceLowerCase = usersChoice.toLowerCase();
+
+    return usersChoiceLowerCase;
+
+}
+
+console.log(getUsersChoice())
